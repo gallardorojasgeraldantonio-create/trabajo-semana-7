24469b1f9750ce094f3e530fd19c64710d330d42
GERALD <gallardorojasgeraldantonio@gmail.com>
2026-05-14 23:38:49 -0500

    Auto: re-upload

--- trabajo-semana-7/Program.cs
+++ trabajo-semana-7/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace trabajo_semana_7
 {
@@ -17,8 +18,8 @@ namespace trabajo_semana_7
             //ejercicio01();
             //ejercicio02();
             //--------tarea (mi parte)---------
+            //ejercicio05();
             ejercicio06();
-            //ejercicio07();
 
 
         }
@@ -139,7 +140,7 @@ namespace trabajo_semana_7
 
             
         }
-        static void ejercicio06()
+        static void ejercicio05() 
         {
             /*
             Reto integrador con estructura repetitiva Hacer Mientras / do-while, números aleatorios, 
@@ -217,6 +218,99 @@ namespace trabajo_semana_7
 
 
 
+        }
+        static void ejercicio06()
+        {
+            /*
+            Sistema de Votación Electrónica Tipo: Reto integrador con estructura repetitiva 
+            Hacer Mientras / do-while, decisión múltiple, contadores y comparación de resultados.  
+            En una elección hay 3 candidatos: 
+            Opción Candidato 
+            1       Ana Torres 
+            2       Carlos Mendoza 
+            3       Lucía Paredes 
+            0       Cerrar votación 
+            Los votantes ingresan su voto uno a uno. Si el voto es 1, 2 o 3, se suma al candidato 
+            correspondiente. Si el voto es otro número diferente de 0, se considera voto nulo y se cuenta por separado. 
+            Al ingresar 0, se cierra la votación. Al finalizar, el programa debe mostrar: 
+            • votos de Ana Torres;  
+            • votos de Carlos Mendoza;  
+            • votos de Lucía Paredes;  
+            • votos nulos;  
+            • total de votantes;  
+            • candidato 
+            ganador o empate. 
+            */
+
+            int ana_torres = 0;
+            int carlos_mendoza = 0;
+            int lucia_paredes = 0;
+            int nulos = 0;
+
+            Console.WriteLine("---------- VOTOS ----------");
+            Console.WriteLine("candidatos");
+            Console.WriteLine("1. Ana Torres");
+            Console.WriteLine("2. Carlos Mendoza");
+            Console.WriteLine("3. Lucía Paredes");
+            Console.WriteLine("0. Cerrar votación");
+            Console.Write("ingrese su voto ------> : ");
+            int voto = int.Parse(Console.ReadLine());
+
+            do
+            {
+ 
+
+                if (voto == 1)
+                {
+                    Console.WriteLine("voto para Ana Torres");
+                    ana_torres++;
+                }
+                else if (voto == 2)
+                {
+                    Console.WriteLine("voto para Carlos Mendoza");
+                    carlos_mendoza++;
+                }
+                else if (voto == 3)
+                {
+                    Console.WriteLine("voto para Lucia Paredes");
+                    lucia_paredes++;
+                }
+                else if (voto != 0)
+                {
+                    Console.WriteLine("voto nulo");
+                    nulos++;
+                }
+                Console.Write("ingrese su voto ------> : ");
+                voto = int.Parse(Console.ReadLine());
+            }
+            while (voto != 0);
+            {
+                int total_votos = ana_torres + carlos_mendoza + lucia_paredes;
+
+                Console.WriteLine($"votos de Ana Torres {ana_torres}");
+                Console.WriteLine($"votos de Carlos Mendoza {carlos_mendoza}");
+                Console.WriteLine($"votos de Lucía Paredes {lucia_paredes}");
+                Console.WriteLine($"votos nulos {nulos}");
+                Console.WriteLine($"total de votantes {total_votos}");
+
+                if (ana_torres > carlos_mendoza && ana_torres > lucia_paredes)
+                {
+                    Console.WriteLine("ganadora es Ana Torres");
+                }
+                else if (carlos_mendoza > ana_torres && carlos_mendoza > lucia_paredes)
+                {
+                    Console.WriteLine("ganador es Carlos Mendoza");
+                }
+                else if (lucia_paredes > ana_torres && lucia_paredes > carlos_mendoza)
+                {
+                    Console.WriteLine("ganadora es Lucia Paredes");
+                }
+                else
+                {
+                    Console.WriteLine("empate no ay ganadores");
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
